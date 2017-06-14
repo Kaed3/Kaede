76115dc6be6224654dd23aa5bc3683fe2d750b41
Alala <Ala1a@users.noreply.github.com>
2017-06-14 14:05:47 +0200

    Create SECRETS.java

--- src/main/java/util/SECRETS.java
+++ src/main/java/util/SECRETS.java
@@ -2,6 +2,6 @@ package util;
 
 public class SECRETS {
 
-    public static final String TOKEN = "MzIzODU5NjM0ODE5NDMyNDUx.DCC9cw.z-F0nurlAbPvwBKZPyc6Un-7azo";
+    public static final String TOKEN = "###############";
 
 }
